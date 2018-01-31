9829ed774bd118f2211e48b764515cdbd157d9ef
ArzuNuriev <arzunuri004@gmail.com>
2018-01-31 14:07:17 +0400

    Github irakli Magari

---
+++ sass/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sass Gamocda</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<p>Gamocda: Irakli Magaria</p>
+	<nav>
+		<ul>
+			<li><a href="javascript:void(0)">HOME</a></li>
+			<li><a href="javascript:void(0)">ABOUT US</a></li>
+			<li><a href="javascript:void(0)">GALLERY</a></li>
+			<li><a href="javascript:void(0)">WORKS</a></li>
+			<li><a href="javascript:void(0)">CARS</a></li>
+		</ul>
+	</nav>
+	<div></div>
+	<div></div>
+	<div></div>
+
+</body>
+</html>
